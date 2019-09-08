af4eb28574238ca6dfbf4f363f116e6b6e18eaaf
removecode <1113955403@qq.com>
2019-09-08 15:51:39 +0800

    首页更新完成

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // 开启eslint支持
+  eslinIntegration: true,
+  // 强制使用单引号
+  singleQuote: true,
+  // 不使用分号结尾
+  semi: false
+}
---
+++ src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">
+    <h1>欢迎来到首页首页</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
+
+
+
+
